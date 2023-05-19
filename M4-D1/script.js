14cefbfeefd167231c1fbc5ed0dff48b17f38a02
Simone Schioppo <simoneschioppo@MBP-di-Simone.station>
2023-05-19 21:06:33 +0200

    esercizio 1 modulo 4

---
+++ M4-D1/script.js
@@ -0,0 +1,91 @@
+//ex.1//
+// const num1 = 50;
+// const num2 = 25 ;
+
+// function adder (a , b){
+//     if (a === 50 || b === 5 || a+b === 5) {
+//         return true
+//     } else {
+//         return false
+//     };
+
+// };
+
+// console.log(adder( num2))
+
+//ex.2//
+
+// const stringaOrigine = "ciao , EPICODE";
+// const rimoction = ",";
+
+// function remover(string , rimoction){
+// return string.replace(rimoction , '');
+// }
+
+
+// const stringaModificata = remover(stringaOrigine,rimoction)
+
+// console.log(stringaModificata);
+
+
+//ex.3//
+// function control (a, b){
+//     if ((a >= 40 && a <= 60)&& (b>= 40 && b <= 60) || (a >= 70 && a <= 100)&& (b>= 70 && b <= 100)) {
+//         return true
+        
+//     } else { return false
+        
+//     };
+
+// }
+
+// const numero1 = 65;
+// const numero2  = 41;
+
+// console.log(control(numero2));
+
+//ex.4//
+// function enamer (string){
+//     if (string.startsWith("los"|| "New")) {
+//         return string
+        
+//     } else { return false
+        
+//     };
+// }
+
+// const city1 = "los Angeles";
+// const city2 = "Napoli";
+// const city3 = "New York";
+
+// console.log(enamer(city2));
+
+//ex.5//
+// function arraySum (array){
+//     let somma = 0;
+//     for (let i = 0; i < array.length; i++) {
+//        somma += array[i];
+//     }
+//     return somma;
+// }
+
+// const myArr = [1,2,3,4,5,];
+// const risultato = arraySum(myArr);
+// console.log(risultato);
+
+//ex.6//
+// function checker (array){
+//     for (let i = 0; i < array.length; i++) {
+        
+//     }if (array[i] !== 1 && array[i] !== 3) {
+//         return true
+//     } else { 
+//         return false
+        
+//     };
+// }
+
+// const check1 = [1,3,5];
+// const check2 = [2,5,9];
+
+// console.log(checker(check2));
